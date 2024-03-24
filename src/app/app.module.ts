3cec6c39f70ce8f5f403edb6e2c82800bd269d8f
Abilov599 <abilovv599@gmail.com>
2024-03-25 01:01:27 +0400

    Refactor app.module.ts to include ConfigModule and UsersModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
+import { DataSource } from "typeorm";
+import { UsersModule } from "./users/users.module";
+import { User } from "./users/user.entity";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        autoLoadEntities: true,
-        ssl:
-          configService.get<string>('NODE_ENV') === 'production'
-            ? { rejectUnauthorized: false }
-            : false,
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot(),TypeOrmModule.forRoot({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl:
+     { rejectUnauthorized: false }
+   ,
+    entities: [User],
+  }), UsersModule],
   providers: [AppService],
+  controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+}
