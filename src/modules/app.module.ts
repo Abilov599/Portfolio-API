27cb637d663803f17ab0a7e531af27d0e6dfb368
Abilov599 <abilovv599@gmail.com>
2024-03-29 22:04:51 +0400

    Add ViewsModule to app.module.ts

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { ViewsModule } from './views/views.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule],
+  imports: [DatabaseModule, UsersModule, AuthModule, ViewsModule],
   providers: [],
   controllers: [],
 })
