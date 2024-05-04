1424fda5cf80d61eab0fce84b344168906d5a31e
Abilov599 <abilovv599@gmail.com>
2024-05-04 11:22:50 +0400

    Refactor CORS configuration and whitelist in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ImATeapotException, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
@@ -6,31 +6,9 @@ import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './modules/app.module';
 
-const whitelist = [
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'http://10.0.2.2:3000',
-];
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: {
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-      origin: function (origin, callback) {
-        if (!origin) {
-          callback(null, true);
-          return;
-        }
-        if (
-          whitelist.includes(origin) ||
-          !!origin.match(/jeyhunabilov\.live$/)
-        ) {
-          callback(null, true);
-        } else {
-          callback(new ImATeapotException('Not allowed by CORS'), false);
-        }
-      },
-    },
+    cors: true,
   });
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
