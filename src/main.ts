e117455ce26fa788c73b0091ad34163604eedec0
Abilov599 <abilovv599@gmail.com>
2024-05-04 11:07:45 +0400

    Update CORS configuration and whitelist in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ImATeapotException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
@@ -6,8 +6,32 @@ import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './modules/app.module';
 
+const whitelist = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'http://10.0.2.2:3000',
+];
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: {
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      origin: function (origin, callback) {
+        if (!origin) {
+          callback(null, true);
+          return;
+        }
+        if (
+          whitelist.includes(origin) ||
+          !!origin.match(/jeyhunabilov\.live$/)
+        ) {
+          callback(null, true);
+        } else {
+          callback(new ImATeapotException('Not allowed by CORS'), false);
+        }
+      },
+    },
+  });
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
